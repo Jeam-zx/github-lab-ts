931f45aa0c8b795d9d1a681847940eea58aaaa01
Jeamhowards Montiel <19-10234@usb.ve>
2024-09-27 20:31:32 +0200

    Arreglar tipado de archivo fib.ts

--- src/fib.ts
+++ src/fib.ts
@@ -1,12 +1,19 @@
 // util function that computes the fibonacci numbers
-module.exports = function fibonacci(n) {
+type fibonacciFunction = (n: number) => number;
+
+/**
+ * @type {fibonacciFunction}
+ */
+const fibonacci: fibonacciFunction = function (n: number): number {
   if (n < 0) {
     return -1;
-  } else if (n == 0) {
+  } else if (n === 0) {
     return 0;
-  } else if (n == 1) {
+  } else if (n === 1) {
     return 1;
   }
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 };
+
+module.exports = fibonacci;
