3c92b6ba7580393e8263af91c5f0111a4abea8b5
Jeamhowards Montiel <19-10234@usb.ve>
2024-09-27 19:57:41 +0200

    Arreglar problema de tipeo en archivo fibRoute.ts

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,20 @@
 // Endpoint for querying the fibonacci numbers
-
+import { Request, Response } from 'express';
 const fibonacci = require("./fib");
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: Request, res: Response) => {
+  if(req.params && req.params.num && typeof req.params.num === "number"){
+    const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+    const fibN = fibonacci(parseInt(num));
+    let result = `fibonacci(${num}) is ${fibN}`;
 
-  if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    if (fibN < 0) {
+      result = `fibonacci(${num}) is undefined`;
+    }
+
+    res.status(200).send(result);
+  } else {
+    res.status(400).send({result: "num no es un numero"})
   }
-
-  res.send(result);
 };
